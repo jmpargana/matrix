9a9f06a25d39ae6b739216746ef34d93312ae35e
jmpargana <jmpargana@stud.uni-frankfurt.de>
2020-06-13 14:56:20 +0200

    added package documentation and fixed stringer

--- matrix_type.go
+++ matrix_type.go
@@ -1,3 +1,8 @@
+// Package matrix is a go package that implements a simple matrix type.
+// It has both methods for the type as well as functions for the most
+// important operations: Mult, Add, Sub, MultScalar, AddScalar and HadamardProd.
+// It uses concurrency only in the matrix multiplication, since its the only
+// place where the overhead might actually be worth it.
 package matrix
 
 // Matrix is a matrix of floats. The size of the matrix is publicly available,
--- stringer.go
+++ stringer.go
@@ -5,6 +5,7 @@ import (
 )
 
 func (m Matrix) String() (s string) {
+	s += fmt.Sprintf("%dx%d", m.NumRows, m.NumCols)
 	for i := 0; i < m.NumRows; i++ {
 		for j := 0; j < m.NumCols; j++ {
 			elem, _ := m.Get(i, j)
