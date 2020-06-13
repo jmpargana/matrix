e67638e7adf06e6cd3aa5f98bb20679fe200b008
jmpargana <jmpargana@stud.uni-frankfurt.de>
2020-06-13 12:14:07 +0200

    more detailed float output with stringer

--- stringer.go
+++ stringer.go
@@ -8,7 +8,7 @@ func (m Matrix) String() (s string) {
 	for i := 0; i < m.NumRows; i++ {
 		for j := 0; j < m.NumCols; j++ {
 			elem, _ := m.Get(i, j)
-			s += fmt.Sprintf("%.2f", elem)
+			s += fmt.Sprintf("%.5f", elem)
 			s += " "
 		}
 		s += "\n"
