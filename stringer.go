6fc2c32e70e93c17722b0ec0961d1d09e257b0c7
jmpargana <jmpargana@stud.uni-frankfurt.de>
2020-06-13 16:26:39 +0200

    indexing

--- stringer.go
+++ stringer.go
@@ -6,6 +6,7 @@ import (
 
 func (m Matrix) String() (s string) {
 	s += fmt.Sprintf("%dx%d", m.NumRows, m.NumCols)
+	s += "\n\n"
 	for i := 0; i < m.NumRows; i++ {
 		for j := 0; j < m.NumCols; j++ {
 			elem, _ := m.Get(i, j)
