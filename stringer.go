1087b39dbbc0d9ece6a380ef45e75590ed960e7b
jmpargana <jmpargana@stud.uni-frankfurt.de>
2020-06-13 16:30:29 +0200

    a little bit better rendering of stringer

--- stringer.go
+++ stringer.go
@@ -6,8 +6,15 @@ import (
 
 func (m Matrix) String() (s string) {
 	s += fmt.Sprintf("%dx%d", m.NumRows, m.NumCols)
-	s += "\n\n"
+	s += "\n\t"
+
+	for i := 0; i < m.NumCols; i++ {
+		s += fmt.Sprintf("col %d\t", i+1)
+	}
+	s += "\n"
+
 	for i := 0; i < m.NumRows; i++ {
+		s += fmt.Sprintf("row %d: ", i+1)
 		for j := 0; j < m.NumCols; j++ {
 			elem, _ := m.Get(i, j)
 			s += fmt.Sprintf("%.5f", elem)
